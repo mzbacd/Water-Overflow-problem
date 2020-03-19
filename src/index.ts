0681eeb66f1a3faf3bd956fca397df0273c15ec8
mzbac <li.anchen.au@gmail.com>
2020-03-20 10:58:20 +1100

    fix input interface

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,17 @@
 import { createInterface, ReadLineOptions } from "readline";
 import { Query, waterOverflowCalc } from "./waterOverflowCalc";
 
-process.stdout.write(
-  `Calc water overflow, input example poured liquid, ith row, jth glass`
-);
-
 const readLineOptions: ReadLineOptions = {
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
+  terminal: false
 };
 
 const readline = createInterface(readLineOptions);
+readline.setPrompt(
+  "Calc water overflow, input example poured liquid, ith row, jth glass\n"
+);
 readline.prompt(true);
-
 readline.on(`line`, line => {
   const [pouredStr, ithRowStr, jthGlassStr] = line.trim().split(",");
   const query: Query = {
@@ -21,10 +20,9 @@ readline.on(`line`, line => {
     queryGlass: parseInt(jthGlassStr)
   };
   const res = waterOverflowCalc(query);
-  console.log(res);
 
-  process.stdout.write(
-    `liquid of ${jthGlassStr} glass of ${ithRowStr} row is ${res}!`
+  readline.setPrompt(
+    `liquid of ${jthGlassStr}th glass of ${ithRowStr}th row is ${res}!\n`
   );
   readline.prompt(true);
 });
